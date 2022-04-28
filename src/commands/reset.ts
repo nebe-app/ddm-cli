8b21403a3ac6aa5b98dd13548fadf1c9580b2f12
Patrik Janosdeak <patrik.janosdeak@isobar.com>
2022-04-28 16:10:38 +0200

    Add reset command

---
+++ src/commands/reset.ts
@@ -0,0 +1,32 @@
+import fs from 'fs-extra';
+import path from 'path';
+import rimraf from 'rimraf';
+import chalk from 'chalk';
+
+import BaseCommand from '../BaseCommand';
+import { getRoot } from '../utils/configGetters';
+
+export class Reset extends BaseCommand {
+	static description = 'Fix some package mismatch issues';
+
+	// hidden from help command
+	static hidden = true;
+
+	async run(): Promise<void> {
+		const root = getRoot();
+
+		rimraf.sync(path.join(root, 'node_modules'));
+		console.log(chalk.green('Deleted ' + path.join(root, 'node_modules')));
+		rimraf.sync(path.join(root, 'package-lock.json'));
+		console.log(chalk.green('Deleted ' + path.join(root, 'package-lock.json')));
+		rimraf.sync(path.join(root, 'yarn.lock'));
+		console.log(chalk.green('Deleted ' + path.join(root, 'yarn.lock')));
+		rimraf.sync(path.join(root, 'package.json'));
+		console.log(chalk.green('Deleted ' + path.join(root, 'package.json')));
+
+		const packageJsonContents = fs.readJsonSync(path.join(__dirname, '..', 'assets', 'packageJsonTemplate.json'));
+
+		fs.writeFileSync(`${root}/package.json`, JSON.stringify(packageJsonContents, null, '\t'));
+		console.log(chalk.green('Created new ' + path.join(root, 'package.json')));
+	}
+}
