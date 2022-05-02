b50f335fb55de966e0bead157f65957c568c6654
Patrik Janosdeak <patrik.janosdeak@isobar.com>
2022-05-02 09:54:38 +0200

    Remove unnecessary parsing of package.json, since it's already injected into the `this.config` variable

--- src/BaseCommand.ts
+++ src/BaseCommand.ts
@@ -1,17 +1,13 @@
 import * as Sentry from '@sentry/node';
-import path from 'path';
 import { Command } from '@oclif/core';
-import { readFileSync } from 'fs';
 
 export default abstract class BaseCommand extends Command {
 	async init(): Promise<void> {
-		const packageJson = JSON.parse(readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
-
 		Sentry.init({
 			dsn: 'https://ac078c61624944c8a56ce290ffdb5dd8@o170788.ingest.sentry.io/5432248',
-			release: `ddm-cli@${packageJson.version}`,
+			release: `ddm-cli@${this.config.pjson.version}`,
 			// @ts-ignore
-			tags: { version: packageJson.version },
+			tags: { version: this.config.pjson.version },
 			environment: process.env.NODE_ENV,
 			config: {
 				captureUnhandledRejections: true
